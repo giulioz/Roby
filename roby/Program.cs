81e6eb15c07fdfc69b6813930182a428589c9a30
giulioz <mail.zausa.giulio@gmail.com>
2016-08-06 17:41:17 +0200

    Added first form

--- roby/Program.cs
+++ roby/Program.cs
@@ -8,15 +8,12 @@ namespace roby
 {
     static class Program
     {
-        /// <summary>
-        /// Punto di ingresso principale dell'applicazione.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WhiteboardForm());
         }
     }
 }
